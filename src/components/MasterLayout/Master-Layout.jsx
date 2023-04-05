3138a3b5e70d9d212d2622849d385def56816fe1
Jayanta Mondal <jayanta551905@gmail.com>
2023-04-05 23:21:29 +0600

    profile picture update

--- src/components/MasterLayout/Master-Layout.jsx
+++ src/components/MasterLayout/Master-Layout.jsx
@@ -47,10 +47,10 @@ const MasterLayout = (props) => {
 
                     <div className="float-right h-auto d-flex">
                         <div className="user-dropdown">
-                            <img className="icon-nav-img icon-nav" src={getUserDetails()} alt=""/>
+                            <img className="icon-nav-img icon-nav" src={getUserDetails()['photo']} alt=""/>
                             <div className="user-dropdown-content ">
                                 <div className="mt-4 text-center">
-                                    <img className="icon-nav-img" src={getUserDetails()} alt=""/>
+                                    <img className="icon-nav-img" src={getUserDetails()['photo']} alt=""/>
                                     <h6>{getUserDetails()['firstName']}</h6>
                                     <hr className="user-dropdown-divider  p-0"/>
                                 </div>
